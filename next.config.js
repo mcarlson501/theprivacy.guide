7ac0d417a4bb7360d3257a7da82b27255528f7d4
Matthew Carlson <Matthew.Carlson.501@gmail.com>
2025-08-31 18:26:38 -0400

    cloudflare pages updates

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
   experimental: {
     appDir: true,
   },
 }
+
+module.exports = nextConfig
